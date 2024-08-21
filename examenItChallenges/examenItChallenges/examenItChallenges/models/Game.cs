5c3b601fa790b7dd1f1ad06683872857b4d5313d
Iljas Leys <r0803440@student.thomasmore.be>
2024-08-21 14:12:35 +0200

    Momenteel af

---
+++ examenItChallenges/examenItChallenges/examenItChallenges/models/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace examenItChallenges.models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Players { get; set; } = new List<string>();
+        public string Duration { get; set; }
+        public string Difficulty { get; set; }
+        public List<string> Reviews { get; set; } = new List<string>();
+    }
+}
